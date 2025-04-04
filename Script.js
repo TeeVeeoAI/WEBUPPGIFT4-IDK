51650b2b8f27cce85ea1069a67d78bcc22d69710
TeeVeeoAI <179211594+TeeVeeoAI@users.noreply.github.com>
2025-04-04 17:36:50 +0200

    Initial commit

---
+++ Script.js
@@ -0,0 +1,12 @@
+function ToggleThemeD(){
+    let html = document.getElementsByTagName('html')[0];
+    html.classList.toggle('theme-light');
+    document.getElementById('moon').style.display = "none";
+    document.getElementById('sun').style.display = "block";
+}
+function ToggleThemeL(){
+    let html = document.getElementsByTagName('html')[0];
+    html.classList.toggle('theme-dark');
+    document.getElementById('mooe').style.display = "block";
+    document.getElementById('sun').style.display = "none";
+}
